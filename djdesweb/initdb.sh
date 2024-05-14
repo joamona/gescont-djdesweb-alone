e64267e0de09174115091ca1ff1f3b47d642b463
joamona <joamona@cgf.upv.es>
2024-05-14 08:04:51 +0200

    venv script

--- djdesweb/initdb.sh
+++ djdesweb/initdb.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 python manage.py makemigrations
 python manage.py migrate
 DJANGO_SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD} python manage.py createsuperuser --noinput --username ${DJANGO_SUPERUSER_USERNAME} --email ${DJANGO_SUPERUSER_EMAIL}
